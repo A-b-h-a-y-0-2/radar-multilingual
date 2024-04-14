8cbbadd73ef2d9876f6b486a68ed598d390bcefb
Dead-Bytes <divyrs999@gmail.com>
2024-04-14 21:56:25 +0530

    addresed #4 in script.py

--- script.py
+++ script.py
@@ -6,6 +6,11 @@ from tqdm import tqdm
 from transformers import pipeline
 import torch.nn.functional as F
 import csv
+from .ai_generate import *
+import os
+import pandas as pd
+import csv
+from sklearn.metrics import accuracy_score, auc, roc_curve
 
 
 def get_args():
@@ -15,18 +20,103 @@ def get_args():
     parser.add_argument('--tr', type=bool, help='choose translation mode', default=False,)
     parser.add_argument('--samples', type=int, help='number off samples to take.')
     parser.add_argument('--ai', type=str, help='path to the ai file')
-    parser.add_argument('--output_h', type=str, help='path to the output file for human preds')
-    parser.add_argument('--output_ai', type=str, help='path to the output file for ai preds')
+    parser.add_argument('--output', type=str, help='path to the output file for human preds')
+    parser.add_argument('--dataset', type=str, help='choose dataset', choices=['wikipedia', 'multitude'])
     return parser.parse_args()
+def analyse(pred_ai, pred_human, output, model):
+    ## Calculate the ROC metrics
+    ## calculate confusion matrix
+    ## calculate accuracy
+    ## calaculate accuracy score \
+    ## save to outpt file
+    pass
 
-def load_data(language, ai, samples):
+def get_roc_metrics(human_preds, ai_preds):
+    # human_preds is the ai-generated probabiities of human-text
+    # ai_preds is the ai-generated probabiities of AI-text
+    if not human_preds or not ai_preds:
+            # Handle empty arrays to avoid the IndexError
+            return None    # Rest of your code
+    fpr, tpr, _ = roc_curve([0] * len(human_preds) + [1] * len(ai_preds), human_preds + ai_preds, pos_label=1)
+    roc_auc = auc(fpr, tpr)
+    return fpr.tolist(), tpr.tolist(), float(roc_auc)
+
+def accuracy(human_pred, ai_pred):
+    count_correct_human = 0
+    count_incorrect_human = 0
+    count_correct_ai = 0
+    count_incorrect_ai = 0
+    for i in human_pred:
+        if i < 0.5:
+            count_correct_human+=1
+        else:
+            count_incorrect_human +=1
+    for i in ai_pred:
+        if i > 0.5:
+            count_correct_ai +=1
+        else:
+            count_incorrect_ai +=1
+    return  (count_correct_human/len(human_pred)), (count_incorrect_human)/len(human_pred), (count_correct_ai/len(ai_pred)), (count_incorrect_ai)/len(ai_pred)
+
+def load_data_multi(model, output):
+    df_main = pd.read_csv('multitude.csv')
+    df = df_main[['label', 'text','language']]
+    languages = ['de', 'en', 'es', 'nl', 'pt', 'ru', 'zh', 'ar', 'uk', 'cs', 'ca']
+    for language in languages:
+        df_test = df[df['language'] == language]
+        df_ai = df_test[df['label'] == 1]
+        df_human = df_test[df['label'] == 0]
+        human = df_human['text'].tolist()
+        ai = df_ai['text'].tolist()
+        if model == 'RADAR':
+            pred_h, pred_ai = analyse_radar(human, ai)
+        elif args.model == 'RoBERTa':
+            pred_h, pred_ai = analyse_roberta(human, ai)
+        elif args.model == 'logrank':
+            pred_h, pred_ai = analyse_logrank(human, ai)
+        elif args.model == 'logp':
+            pred_h, pred_ai = analyse_logp(human, ai)
+
+        analyse(pred_ai, pred_h, output, model)
+
+
+    
+
+def load_data_wiki(language, samples):
     human_text = [] 
     ai_text = []
+    ai_gen = False
+    model = input('Select model to generate ai corpus (Vicuna, llama2, None)')
     if language == 'French':
         datawiki_fr = load_dataset('wikipedia', '20220301.fr')
         for i in range(samples):
             human_text.append(datawiki_fr['train'][i]['text'])
-        with open(ai,'r') as f:
+    elif language == 'German':
+        datawiki_de = load_dataset('wikipedia', '20220301.de')
+        for i in range(samples):
+            human_text.append(datawiki_de['train'][i]['text'])
+    elif language == 'Italian':
+        datawiki_it = load_dataset('wikipedia', '20220301.it')
+        for i in range(samples):
+            human_text.append(datawiki_it['train'][i]['text'])
+    else:
+        print('Language not supported')
+    
+    
+    if model == 'Vicuna':
+        ai_gen = True
+    elif model == 'llama2':
+        ai_gen = True
+    filepath = model+'_ai.txt'
+    if ai_gen:
+        if model == 'Vicuna':
+            vicuna(human, language, filepath, 'cuda')
+        elif model == 'llama2':
+            llama(human, language, filepath, 'cuda')
+    
+    
+    if language == 'French':
+        with open(filepath,'r') as f:
             line = f.readline()
             while line:
                 line = line.strip()
@@ -38,10 +128,7 @@ def load_data(language, ai, samples):
                 ai_text.append(line)
                 line = f.readline()
     elif language == 'German':
-        datawiki_de = load_dataset('wikipedia', '20220301.de')
-        for i in range(samples):
-            human_text.append(datawiki_de['train'][i]['text'])
-        with open(ai,'r') as f:
+        with open(filepath,'r') as f:
             line = f.readline()
             while line:
                 line = line.strip()
@@ -53,10 +140,7 @@ def load_data(language, ai, samples):
                 ai_text.append(line)
                 line = f.readline()
     elif language == 'Italian':
-        datawiki_it = load_dataset('wikipedia', '20220301.it')
-        for i in range(samples):
-            human_text.append(datawiki_it['train'][i]['text'])
-        with open(ai,'r') as f:
+        with open(filepath,'r') as f:
             line = f.readline()
             while line:
                 line = line.strip()
@@ -67,8 +151,6 @@ def load_data(language, ai, samples):
                 line = line.replace('\']',' ')
                 ai_text.append(line)
                 line = f.readline()
-    else:
-        print('Language not supported')
     ai_text = ai_text[:samples]
     return human_text, ai_text
 
@@ -83,52 +165,42 @@ def analyse_radar(human, ai, output_h, output_ai):
 
     Text_input = human
     # error = 0
-    output_probs_list =[]
+    output_probs_list_human =[]
     # Use detector to deternine wehther the text_input is ai-generated.
     with torch.no_grad():
         for i  in tqdm(Text_input):
             inputs = detector_tokenizer(i, padding=True, truncation=True, max_length=512, return_tensors="pt")
             inputs = {k:v.to(device) for k,v in inputs.items()}
             output_probs = F.log_softmax(detector(**inputs).logits,-1)[:,0].exp().tolist()
-            output_probs_list.append(output_probs)
-    with open(output_h, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs_list:
-            writer.writerow(item)
+            output_probs_list_human.append(output_probs)
     
     Text_input = ai
-    output_probs_list =[]
+    output_probs_list_ai =[]
     # Use detector to deternine wehther the text_input is ai-generated.
     with torch.no_grad():
         for i  in tqdm(Text_input):
             inputs = detector_tokenizer(i, padding=True, truncation=True, max_length=512, return_tensors="pt")
             inputs = {k:v.to(device) for k,v in inputs.items()}
             output_probs = F.log_softmax(detector(**inputs).logits,-1)[:,0].exp().tolist()
-            output_probs_list.append(output_probs)
-    with open(output_ai, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs_list:
-            writer.writerow(item)
+            output_probs_list_ai.append(output_probs)
+
+    return output_probs_list_human, output_probs_list_ai
 
 def analyse_roberta(human, ai, output_h, output_ai):
     pipe = pipeline("text-classification", model="openai-community/roberta-large-openai-detector")
-    output_probs = []
+    output_probs_human = []
     for i in tqdm(human):
         output = pipe(i)
-        output_probs.append(output)
-    with open(output_h, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
+        output_probs_human.append(output)
+
     
-    output_probs = []
+    output_probs_ai = []
     for i in tqdm(ai):
         output = pipe(i)
-        output_probs.append(output)
-    with open(output_ai, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
+        output_probs_ai.append(output)
+
+    return output_probs_human, output_probs_ai
+    
 
 def get_rank(text, base_model, base_tokenizer,log=False ):
     DEVICE = 'cuda'
@@ -174,69 +246,51 @@ def analyse_logrank(human, ai, output_h, output_ai):
     base_model = transformers.AutoModelForCausalLM.from_pretrained("openai-community/gpt2-medium")
     base_model.eval()
     base_model.to('cuda')
-    output_probs = []
+    output_probs_human = []
     for i in tqdm(human):
         output = get_rank(i, base_model, base_tokenizer)
-        output_probs.append(output)
-    with open(output_h, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
+        output_probs_human.append(output)
 
-    output_probs = []
+
+    output_probs_ai = []
     for i in tqdm(ai):
         output = get_rank(i, base_model, base_tokenizer)
-        output_probs.append(output)
-    with open(output_ai, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
+        output_probs_ai.append(output)
+
+    return output_probs_human, output_probs_ai
 
 def analyse_logp(human, ai, output_h, output_ai):
     base_tokenizer = transformers.AutoTokenizer.from_pretrained("openai-community/gpt2-medium")
     base_model = transformers.AutoModelForCausalLM.from_pretrained("openai-community/gpt2-medium")
     base_model.eval()
     base_model.to('cuda')
-    output_probs = []
+    output_probs_human = []
     for i in tqdm(human):
         output = get_ll(i, base_tokenizer, base_model)
-        output_probs.append(output)
-    with open(output_h, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
-
-    output_probs = []
+        output_probs_human.append(output)
+    
+    output_probs_ai = []
     for i in tqdm(ai):
         output = get_ll(i, base_tokenizer, base_model)
-        output_probs.append(output)
-    with open(output_ai, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
+        output_probs_ai.append(output)
+    return output_probs_human, output_probs_ai
 
 def analyse_entropy(human, ai, output_h, output_ai):
     base_tokenizer = transformers.AutoTokenizer.from_pretrained("openai-community/gpt2-medium")
     base_model = transformers.AutoModelForCausalLM.from_pretrained("openai-community/gpt2-medium")
     base_model.eval()
     base_model.to('cuda')
-    output_probs = []
+    output_probs_human = []
     for i in tqdm(human):
         output = get_entropy(i, base_tokenizer, base_model)
-        output_probs.append(output)
-    with open(output_h, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
+        output_probs_human.append(output)
+ 
 
-    output_probs = []
+    output_probs_ai = []
     for i in tqdm(ai):
         output = get_entropy(i, base_tokenizer, base_model)
-        output_probs.append(output)
-    with open(output_ai, 'w', newline= '\n') as f:
-        writer = csv.writer(f)
-        for item in output_probs:
-            writer.writerow(item)
+        output_probs_ai.append(output)
+    return output_probs_human, output_probs_ai
             
 def translate(text, language):
     tokenizer = None 
@@ -278,40 +332,33 @@ if __name__ == '__main__':
         human = translate(human, args.language)
         ai = translate(ai, args.language)
     if args.model == 'RADAR':
-        output_h = os.path.join(args.output_h, 'RADAR_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'RADAR_'+ args.language +'_ai.csv')
-        analyse_radar(human, ai, output_h, output_ai)
+        pred_h, pred_ai = analyse_radar(human, ai)
+        analyse(pred_ai, pred_h, args.output, args.model)
     elif args.model == 'RoBERTa':
-        output_h = os.path.join(args.output_h, 'RoBERTa_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'RoBERTa_'+ args.language +'_ai.csv')
-        analyse_roberta(human, ai, output_h, output_ai)
+        pred_h, pred_ai = analyse_roberta(human, ai)
+        analyse(pred_ai, pred_h, args.output, args.model)
     elif args.model == 'logrank':
-        output_h = os.path.join(args.output_h, 'logrank_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'logrank_'+ args.language +'_ai.csv')
-        analyse_logrank(human, ai, args.output_h, args.output_ai)
+        pred_h, pred_ai = analyse_logrank(human, ai)
+        analyse(pred_ai, pred_h, args.output, args.model)
     elif args.model == 'logp':
-        output_h = os.path.join(args.output_h, 'logp_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'logp_'+ args.language +'_ai.csv')
-        analyse_logp(human, ai, args.output_h, args.output_ai)
+        pred_h, pred_ai = analyse_logp(human, ai)
+        analyse(pred_ai, pred_h, args.output, args.model)
     elif args.model == 'entropy':
-        output_h = os.path.join(args.output_h, 'entropy_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'entropy_'+ args.language +'_ai.csv')
-        analyse_entropy(human, ai, args.output_h, args.output_ai)
+        pred_h, pred_ai = analyse_entropy(human, ai)
+        analyse(pred_ai, pred_h, args.output, args.model)
     elif args.model == 'all':
-        output_h = os.path.join(args.output_h, 'RADAR_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'RADAR_'+ args.language +'_ai.csv')
-        analyse_radar(human, ai, output_h, output_ai)
-        output_h = os.path.join(args.output_h, 'RoBERTa_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'RoBERTa_'+ args.language +'_ai.csv')
-        analyse_roberta(human, ai, output_h, output_ai)
-        output_h = os.path.join(args.output_h, 'logrank_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'logrank_'+ args.language +'_ai.csv')
-        analyse_logrank(human, ai, args.output_h, args.output_ai)
-        output_h = os.path.join(args.output_h, 'logp_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'logp_'+ args.language +'_ai.csv')
-        analyse_logp(human, ai, args.output_h, args.output_ai)
-        output_h = os.path.join(args.output_h, 'entropy_'+args.language+'_human.csv')
-        output_ai = os.path.join(args.output_ai, 'entropy_'+ args.language +'_ai.csv')
-        analyse_entropy(human, ai, args.output_h, args.output_ai)
+        radar_pred_h, radar_pred_ai = analyse_radar(human, ai)
+        roberta_pred_h, roberta_pred_ai = analyse_roberta(human, ai)
+        logrank_pred_h, logrank_pred_ai =analyse_logrank(human, ai)
+        logp_pred_h, logp_pred_ai = analyse_logp(human, ai)
+        entropy_pred_h, entropy_pred_ai = analyse_entropy(human, ai)
+        analyse(radar_pred_ai, radar_pred_h, args.output, 'RADAR')
+        analyse(roberta_pred_ai, roberta_pred_h, args.output, 'RoBERTa')
+        analyse(logrank_pred_ai, logrank_pred_h, args.output, 'logrank')
+        analyse(logp_pred_ai, logp_pred_h, args.output, 'logp')
+        analyse(entropy_pred_ai, entropy_pred_h, args.output, 'entropy')
+
+
+
     
     
